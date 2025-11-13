aae1c224e62b155944f5dd754e589f6550e8fe6f
SARSA002 <sarsa002@mymail.unisa.edu.au>
2025-11-13 20:05:06 +1030

    Implement Functionality for PreprocTextArea.jsx and PlayButtons.jsx
    Integrated React state and event handling for PreprocTextArea.jsx and PlayButtons.jsx within App.js, replacing the previous functionality. Also, fixed minor issues in DJControls (replaced class to className and for to htmlFor for proper React syntax.

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -1,24 +1,24 @@
 function DJControls() {
   return (
     <>
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="cpm_label">setCPM</span>
-            <input type="text" class="form-control" id="cpm_text_input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label"/>
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="cpm_label">setCPM</span>
+            <input type="text" className="form-control" id="cpm_text_input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label"/>
         </div>
 
-        <label for="volume_range" className="form-label">Volume</label>
+        <label htmlFor="volume_range" className="form-label">Volume</label>
         <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
 
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="s1"/>
-                <label class="form-check-label" for="s1">
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="s1"/>
+                <label className="form-check-label" htmlFor="s1">
                     s1
                 </label>
         </div>
         
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="d1"/>
-                <label class="form-check-label" for="d1">
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="d1"/>
+                <label className="form-check-label" htmlFor="d1">
                     d1
                 </label>
         </div>
